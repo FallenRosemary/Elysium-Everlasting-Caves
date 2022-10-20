d015870783d51ea8b0b6a62dd1dd366b2e1d2877
Elysian Rosemary <wmaakor3@gmail.com>
2022-10-20 15:33:55 +0200

    moved item registry over into a constant

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -68,108 +68,161 @@ onEvent('item.registry', event => {
 		{
 			id:`food_lootbox`,
 			name:`Farmer's Delight`,
-			texturePath:`kits`
+			texturePath:`kits/food_lootbox`
 		},
 		{
 			id:`loot_lootbox`,
 			name:`Scavenger's Delight`,
-			texturePath:`kits`
+			texturePath:`kits/loot_lootbox`
 		},
 		{
 			id:`misc_lootbox`,
 			name:`A Delight`,
-			texturePath:`kits`
+			texturePath:`kits/misc_lootbox`
 		},
 		// Crafting Ingredients
 		{
 			id:`copper_piping`,
 			name:`Copper Piping`,
-			texturePath:`kits`
+			texturePath:`misc/copper_piping`
 		},
 		{
 			id:`weird_mechanism`,
 			name:`Weirdly Useful Mechanism`,
-			texturePath:`kits`
+			texturePath:`misc/weird_mechanism`
 		},
 		{
 			id:`storage_core`,
 			name:`Storage Core`,
-			texturePath:`kits`
+			texturePath:`misc/storage_core`
 		},
 		{
 			id:`machine_scraps`,
 			name:`Machine Scraps`,
-			texturePath:`kits`
+			texturePath:`misc/machine_scraps`
+		},
+		{
+			id:`mortar`,
+			name:`Mortar`,
+			texturePath:`misc/mortar`
+		},
+		{
+			id:`storage_essence`,
+			name:`Essence Of Storing`,
+			texturePath:`misc/storage_essence`
+		},
+		{
+			id:`limestone_flux`,
+			name:`Limestone Flux`,
+			texturePath:`misc/limestone_flux`
 		},
 		// One-Off Ingots
 		{
-			id:`superheated_steel`,
+			id:`superheated_steel_ingot`,
 			name:`Superheated Steel Compound`,
-			texturePath:`ingots`
+			texturePath:`ingots/superheated_steel`
 		},
 		{
-			id:`hot_compressed_iron`,
+			id:`hot_compressed_iron_ingot`,
 			name:`Hot Compressed Iron Compound`,
-			texturePath:`ingots`
+			texturePath:`ingots/hot_compressed_iron`
 		},
 		{
-			id:`matter_plastics`,
+			id:`matter_plastics_ingot`,
 			name:`Matter Plastics Sheet`,
-			texturePath:`ingots`
+			texturePath:`ingots/matter_plastics`
 		},
 		{
-			id:`iron_nickel`,
+			id:`iron_nickel_ingot`,
 			name:`Iron-Nickel Compound`,
-			texturePath:`ingots`
+			texturePath:`ingots/iron_nickel`
 		},
 		// Molds
 		{
 			id:`gear_mold`,
 			name:`Gear Mold`,
-			texturePath:`molds`
+			texturePath:`molds/gear`
 		},
 		{
 			id:`screw_mold`,
 			name:`Screw Mold`,
-			texturePath:`molds`
+			texturePath:`molds/screw`
 		},
 		{
 			id:`plate_mold`,
 			name:`plate Mold`,
-			texturePath:`molds`
+			texturePath:`molds/plate`
+		},
+		// One-Off Bricks/Blends/Pastes
+		{
+			id:`seared_blend`,
+			name:`Seared Blend`,
+			texturePath:`misc/seared_blend`
+		},
+		{
+			id:`scorched_blend`,
+			name:`Scorched Blend`,
+			texturePath:`misc/scorched_blend`
+		},
+		{
+			id:`blast_blend`,
+			name:`Blast Blend`,
+			texturePath:`misc/blast_blend`
+		},
+		{
+			id:`coke_blend`,
+			name:`Coke Blend`,
+			texturePath:`misc/coke_blend`
+		},
+		{
+			id:`heat_paste`,
+			name:`Heat-Proof Paste`,
+			texturePath:`misc/heat_paste`
+		},
+		{
+			id:`frost_paste`,
+			name:`Frost-Proof Paste`,
+			texturePath:`misc/frost_paste`
+		},
+		{
+			id:`blast_brick`,
+			name:`Blast Brick`,
+			texturePath:`misc/blast_brick`
+		},
+		{
+			id:`coke_brick`,
+			name:`Coke Brick`,
+			texturePath:`misc/coke_brick`
+		},
+		// Screws
+		{
+			id:`screw_basic`,
+			name:`Crude Screw`,
+			texturePath:`screw/basic`
+		},
+		{
+			id:`screw_brass_incomplete`,
+			name:`Incomplete Brass Screw`,
+			texturePath:`screw/brass_incomplete`
+		},
+		{
+			id:`screw_brass`,
+			name:`Hex Ash-Tipped Brass Screw`,
+			texturePath:`screw/brass`
+		},
+		{
+			id:`screw_compressed_iron`,
+			name:`Compressed Iron Screw`,
+			texturePath:`screw/compressed_iron`
+		},
+		{
+			id:`screw_stainless_steel`,
+			name:`Stainless Steel Screw`,
+			texturePath:`screw/stainless_steel`
 		},
 	]
 
 	basicItemRegistry.forEach((item) => {
-        event.create(`${item.id}`).displayName(`${item.name}`).texture(`kubejs:item/${item.texturePath}/${item.id}`);
+        event.create(`${item.id}`).displayName(`${item.name}`).texture(`kubejs:item/${item.texturePath}`);
     });
-
-	// Blends
-	event.create('seared_brick_blend').displayName("Seared Blend")//
-	event.create('scorched_brick_blend').displayName("Scorched Blend")//
-	event.create('blast_brick_blend').displayName("Blast Blend")// 
-	event.create('coke_brick_blend').displayName("Coke Blend")// 
-
-	event.create('fire_brick_blend').displayName("Heat-Proof Paste")//
-	event.create('frost_brick_blend').displayName("Frost-Proof Paste")//
-
-	// Bricks
-	event.create('blast_brick').displayName("Blast Brick")//
-	event.create('coke_brick').displayName("Coke Brick")//
-
-	// In-world Materials
-
-	// Processing Materials
-	event.create('mortar').displayName("Mortar")//
-	event.create('crushed_storage_essence').displayName("Crushed Storage Essence")
-	event.create('andesite_ashes').displayName("Andesite Ashes")//
-	event.create('limestone_flux').displayName("Limestone Flux")//
-	event.create('dried_straw').displayName("Dried Straw")//
-
-	// Screws
-	event.create('screw_basic').displayName("Crude Screw").texture(`kubejs:item/screws/basic`)
-	event.create('screw_brass').displayName("Hex Ash-Tipped Brass Screw").texture(`kubejs:item/screws/brass`)
-	event.create('screw_brass_incomplete').displayName("Incomplete Brass Screw").texture(`kubejs:item/screws/brass_incomplete`)
-	event.create('screw_compressed_iron').displayName("Compressed Iron Screw").texture(`kubejs:item/screws/compressed_iron`)
-	event.create('screw_stainless_steel').displayName("Stainless Steel Screw").texture(`kubejs:item/screws/stainless_steel`)
 })
