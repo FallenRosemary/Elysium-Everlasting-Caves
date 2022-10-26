95300f81fbec262b71442b70e0e6ce81f1beb930
Elysian Rosemary <wmaakor3@gmail.com>
2022-10-26 18:09:59 +0200

    fluid registry

---
+++ kubejs/startup_scripts/fluid_registry.js
@@ -0,0 +1,8 @@
+onEvent('fluid.registry', event => {
+       
+    event.create('crystal_growing_solution')
+      .thinTexture(0xFF0000)
+      .bucketColor(0x00FFFF)
+      .displayName('Sky-Salt Solution')
+    
+  })
